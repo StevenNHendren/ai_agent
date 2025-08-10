e8c389a1fb3b7229424f182b912ed7f2939d8d94
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-10 13:18:56 -0400

    Update get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,12 +6,23 @@ def get_files_info(working_directory, directory="."):
         abs_p_wd = os.path.abspath(working_directory)
         abs_p_d = os.path.abspath(direcotry)
         if abs_p_wd == os.path.commonpath([abs_p_wd, abs_p_d]):
-            pass
+            my_dir = os.path.join(working_directory, diretory)
+            if os.path.exists(my_dir):
+                items = os.listdir(my_dir)
+                if len(items) > 0:
+                    for item in items:
+                        if os.path.isfile(item):
+                            try:
+                                fs = os.path.getsize(item)
+                            except OSError:
+                                return f"Error: cannot get {item) size"
+                            
+                else:
+                    return f"Error: no items found in {directory}"
+            else:
+                f'Error: "{directory}" does not exist'
         else:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     else:
         return f'Error: "{directory}" is not a directory'
-    if os.path.exists(absolute_path):
-        pass
-    else:
-        f'Error: "{directory}" does not exist'
+
