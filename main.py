eadf9ae4da1e879b6cccdfa9064618f6699cef21
StevenNHendren <tebtengri@gmail.com>
2025-08-10 13:34:41 +0000

    begin project

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+
+def main():
+    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    if (len(sys.argv) > 1):
+        user_prompt = sys.argv[1]
+        if (user_prompt != ""):
+            messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+            ]
+            response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            )
+        else:
+            print("Error: prompt not provided")
+            sys.exit(1)
+    else:
+        print("Error: prompt not provided")
+        sys.exit(1)
+    x = response.usage_metadata.prompt_token_count
+    y = response.usage_metadata.candidates_token_count
+    print(response.text)
+    if (len(sys.argv) > 2):
+        if (sys.argv[2] == "--verbose"):
+            print (f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {x}")
+            print(f"Response tokens: {y}")
+
+if __name__ == "__main__":
+    main()
