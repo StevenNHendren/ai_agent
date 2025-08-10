2f5a8b7949b04d82e42dd4b784d2014a9aafacc4
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-10 16:15:02 -0400

    Update get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -38,5 +38,5 @@ def get_files_info(working_directory, directory="."):
     else:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory\n'
 
-    return retsrring
+    return retstring
 
