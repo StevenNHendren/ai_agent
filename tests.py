9a43115c904f278bc1f82849cc69529bb0d19751
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-10 15:50:22 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,9 +1,12 @@
 from functions.get_files_info import get_files_info
 
-get_files_info("calculator", ".")
+def main();
+  retstring = get_files_info("calculator", ".")
 
-get_files_info("calculator", "pkg")
+  retstring += get_files_info("calculator", "pkg")
 
-get_files_info("calculator", "/bin")
+  retstring += get_files_info("calculator", "/bin")
 
-get_files_info("calculator", "../")
+  retstring += get_files_info("calculator", "../")
+  
+  print(retstring)
