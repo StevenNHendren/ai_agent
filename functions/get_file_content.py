4aceac7f14b31aa09d78edd58be4911a486770f2
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-11 11:25:14 -0400

    Update get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,3 +1,6 @@
 def get_file_content(working_directory, file_path):
   retstring = ""
   my_file = os.path.join(working_directory, file_path)
+
+  if abs_p_d.startswith(os.path.abspath(my_dir)):
+    pass
