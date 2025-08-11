d3cd1e388af8bdb509bc753c3752119e8455b140
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-11 12:59:47 -0400

    Update run_python.py

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 def run_python_file(working_directory, file_path, args=[]):
   abs_p_wd = os.path.abspath(working_directory)
@@ -7,7 +8,10 @@ def run_python_file(working_directory, file_path, args=[]):
   if abs_p_d.startswith(abs_p_wd):
     if os.path.exists(my_dir):
       if file_path.endswith(".py"):
-        pass
+        try:
+          pass
+        catch Exception as e:
+          return f"Error: executing Python file: {e}"
       else:
         return f'Error: "{file_path}" is not a Python file.'  
     else:
@@ -15,32 +19,7 @@ def run_python_file(working_directory, file_path, args=[]):
   else:
     return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
 
-    
-
-    
-    
-
-        
-            if os.path.isdir(my_dir):
-                items = os.listdir(my_dir)
-                if len(items) > 0:
-                    retstring = "Result for current directory:\n"
-                    for item in items:
-
-                        try:
-                            is_dir = os.path.isdir(os.path.join(my_dir, item))
-
-                            fs = os.path.getsize(os.path.join(my_dir, item))
-
-                            fn= str(item)
-                       
-                            retstring += f" - {fn}: file_size={fs} bytes, is_dir={is_dir}\n"
-                        except OSError:
-                            return f"Error: cannot get {item} size\n"
-                else:
-                    return f"Error: no items found in {directory}\n"
-            else:
-                return f'Error: "{directory}" is not a directory\n'        
+      
 
 
 
