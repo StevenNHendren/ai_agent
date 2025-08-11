dd3c8d8ef4f94f5b3cab9787943f7b74f1e852f2
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-11 12:16:00 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
 from functions.get_file_content import get_file_content
 
 def main():
-  retstring = get_file_content("calculator", "main.py")
-  retstring += get_file_content("calculator", "pkg/calculator.py")
-  retstring += get_file_content("calculator", "/bin/cat")
-  retstring += get_file_content("calculator", "pkg/does_not_exist.py")
+  retstring = get_file_content("calculator", "main.py") + "\n"
+  retstring += get_file_content("calculator", "pkg/calculator.py") + "\n"
+  retstring += get_file_content("calculator", "/bin/cat") + "\n"
+  retstring += get_file_content("calculator", "pkg/does_not_exist.py") + "\n"
   print(retstring)
 
 main()
