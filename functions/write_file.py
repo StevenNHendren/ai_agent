3b383b967f8e86da33d343f7cf400c6bd9fb08c3
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-11 12:25:50 -0400

    Update write_file.py

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,2 +1,20 @@
+import os
+
 def write_file(working_directory, file_path, content):
-  pass
+  retstring = ""
+  my_file = os.path.join(working_directory, file_path)
+  abs_my_file = os.path.abspath(my_file)
+  if abs_my_file.startswith(os.path.abspath(working_directory)):
+    try:
+      with open(my_file, "r") as f:
+        retstring = f.read(MAX_CHARS)
+      except FileNotFoundError:
+        return(f"Error: The file '{my_file}' was not found.")
+      except PermissionError:
+        return(f"Error: You do not have permission to access '{my_file}'.")
+      except Exception as e:
+        return(f"Error: An unexpected error occurred: {e}")
+    return retstring + oversize
+  else:
+    return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
