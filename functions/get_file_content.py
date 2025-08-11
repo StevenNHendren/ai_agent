8275e1ee0fe6c0130ec47a13f1621ec982f3250b
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-11 11:31:57 -0400

    Update get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,8 +1,15 @@
+import os
+
 def get_file_content(working_directory, file_path):
   retstring = ""
   my_file = os.path.join(working_directory, file_path)
 
   if abs_p_d.startswith(os.path.abspath(my_dir)):
-    pass
+    if os.path.isfile(my_file):
+      MAX_CHARS = 10000
+      with open(file_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+    else:
+      return f'Error: File not found or is not a regular file: "{file_path}"'
   else:
-    f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
