42d6030ca4f8dde93aee33506e605e5b993a350c
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-11 11:59:44 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,13 +1,7 @@
 from functions.get_files_info import get_files_info
 
 def main():
-  retstring = get_files_info("calculator", ".")
-
-  retstring += get_files_info("calculator", "pkg")
-
-  retstring += get_files_info("calculator", "/bin")
-
-  retstring += get_files_info("calculator", "../")
+  retstring = get_file_content("calculator", "lorem.txt")
   
   print(retstring)
 
