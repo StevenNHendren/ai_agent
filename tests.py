8228495587e667e03305251bac18086460de3f12
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-12 11:04:31 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ def main():
   retstring = run_python_file("calculator", "main.py") + "\n"
   retstring += run_python_file("calculator", "main.py", ["3 + 5"]) + "\n"
   retstring += run_python_file("calculator", "tests.py") + "\n"
+  retstring += run_python_file("calculator", "../main.py") + "\n"
   retstring += run_python_file("calculator", "nonexistent.py") + "\n"
   print(retstring)
 
