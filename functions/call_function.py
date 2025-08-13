bae6bc79f290bf7d763c85c6a6b741cc3a8cf3a0
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-13 16:29:38 -0400

    Update call_function.py

--- functions/call_function.py
+++ functions/call_function.py
@@ -18,7 +18,7 @@ valid_functions = {
 
 def call_function(function_call_part, verbose=False):
     if verbose:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        print(f"Calling function: {function_call_part.name}\({function_call_part.args}\)")
     else:
         print(f" - Calling function: {function_call_part.name}")
 
