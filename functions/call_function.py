18bf2d5f498a7ec43d2006243b81a3b68dc4ff92
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-13 15:02:01 -0400

    Update call_function.py

--- functions/call_function.py
+++ functions/call_function.py
@@ -8,3 +8,25 @@ def call_function(function_call_part, verbose=False):
     print(f"Calling function: {function_call_part.name}({function_call_part.args})")
   else:
     print(f" - Calling function: {function_call_part.name}")
+
+  #if function name is invalid
+  return types.Content(
+    role="tool",
+    parts=[
+        types.Part.from_function_response(
+            name=function_name,
+            response={"error": f"Unknown function: {function_name}"},
+        )
+    ],
+  )
+
+  
+  return types.Content(
+      role="tool",
+      parts=[
+          types.Part.from_function_response(
+              name=function_name,
+              response={"result": function_result},
+          )
+      ],
+  )
