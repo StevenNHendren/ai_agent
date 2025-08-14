948a11c8e3382809f18e872d88c4a6ec29290394
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-14 10:50:04 -0400

    Update call_function.py

--- functions/call_function.py
+++ functions/call_function.py
@@ -37,7 +37,7 @@ def call_function(function_call_part, verbose=False):
 
     function = valid_functions[function_name]
     arg_wd = { "working_directory": "./calculator" }
-    function_result = function(**arg_wd, *args)
+    function_result = function(*args, **arg_wd)
     return types.Content(
       role="tool",
       parts=[
