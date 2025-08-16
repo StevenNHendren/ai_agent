b4fdffe44e9c0dda7c158da84ef85db4524a16a4
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-16 12:58:15 -0400

    Update testwrite.py

--- testwrite.py
+++ testwrite.py
@@ -1,3 +1,3 @@
-write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
