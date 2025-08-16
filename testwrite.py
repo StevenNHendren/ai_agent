fcd13a61cda1fc42effaddb056bac4ef5c2fa2fc
StevenNHendren <144388263+StevenNHendren@users.noreply.github.com>
2025-08-16 13:00:33 -0400

    Update testwrite.py

--- testwrite.py
+++ testwrite.py
@@ -1,3 +1,3 @@
-print(runctions.write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+print(functions.write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 print(functions.write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
 print(functions.write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
